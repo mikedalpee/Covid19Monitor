cd9763ac164a05ae39301d77c6486c002dc3e4a4
mike <mike@loopvoc.com>
2020-03-23 18:37:26 -0400

    Upates to deal with copying database and creating docker image

--- scripts/start_puma.sh
+++ scripts/start_puma.sh
@@ -10,4 +10,4 @@ fi
 source ${HOME}/.bash_profile
 script_dir=$(dirname "$0")
 log_dir=${script_dir}/../log
-bundle exec puma 2>&1 >> `${script_dir}/save_file.sh ${log_dir}/puma_3000 log`&
+bundle exec rails s 2>&1 >> `${script_dir}/save_file.sh ${log_dir}/puma_3000 log`&
