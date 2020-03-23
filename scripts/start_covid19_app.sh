2c3dc8f3d716681efb17c0d57bb1bad42e94c01b
mike <mike@loopvoc.com>
2020-03-23 17:43:57 -0400

    Upates to deal with copying database and creating docker image

--- scripts/start_covid19_app.sh
+++ scripts/start_covid19_app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-source ${HOME}/.bash_profile
 script_dir=$(dirname "$0")
 ${script_dir}/start_postgresql.sh
 ${script_dir}/start_redis.sh
--- scripts/start_puma.sh
+++ scripts/start_puma.sh
@@ -7,6 +7,7 @@ then
   exit 0
 fi
 
+source ${HOME}/.bash_profile
 script_dir=$(dirname "$0")
 log_dir=${script_dir}/../log
 bundle exec rails s 2>&1 >> `${script_dir}/save_file.sh ${log_dir}/puma_3000 log`&
