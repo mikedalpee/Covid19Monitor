3545aa1fbc4b1dc5ecc37e244e7945582d098d96
mike <mike@loopvoc.com>
2020-03-23 09:27:19 -0400

    Upates to deal with copying database and creating docker image

--- scripts/start_covid19_app.sh
+++ scripts/start_covid19_app.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source ${HOME}/.bash_profile
 script_dir=$(dirname "$0")
 ${script_dir}/start_postgresql.sh
 ${script_dir}/start_redis.sh
--- scripts/start_covid19_server.sh
+++ scripts/start_covid19_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --network host -p 3000:3000/tcp covid19-server
+docker run -d -rm -p 3000:3000/tcp covid19-server
