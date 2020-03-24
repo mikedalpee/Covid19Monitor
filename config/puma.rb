ce32ae38760870714d8449ed2e99a333a80916c9
mike <mike@loopvoc.com>
2020-03-23 20:45:50 -0400

    Upates to deal with copying database and creating docker image

--- config/puma.rb
+++ config/puma.rb
@@ -20,7 +20,7 @@ port        ENV.fetch("PORT") { default_port }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "/tmp/pids/server#{default_port}.pid" }
+pidfile ENV.fetch("PIDFILE") { "/tmp/server#{default_port}.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
